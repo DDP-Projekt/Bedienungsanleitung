de109ca32fa8c1b1ff686db8bd200e9ee0da753a
NotLe0n <notle0n75@gmail.com>
2022-06-11 18:10:10 +0200

    responsive header font size, switched to hamburger

--- script.js
+++ script.js
@@ -12,24 +12,25 @@ let sidebarHidden = false;
 function openNav() {
     sidebarHidden = false;
     document.getElementById("seitenleiste").style.display = "flex";
+    
+    const burger = document.getElementById("hamburger");
+    burger.style.left = "calc(var(--sidebar-size) - 20px)";
 }
 
 function closeNav() {
     sidebarHidden = true;
     document.getElementById("seitenleiste").style.display = "none";
+    
+    const burger = document.getElementById("hamburger");
+    burger.style.left = "10px";
 }
 
 function toggleNav() {
-    const pfeil = document.getElementById("pfeil");
     if (sidebarHidden) {
         openNav();
-        pfeil.style.left = "calc(var(--sidebar-size) - 20px)";
-        pfeil.innerHTML = "←"
     }
     else {
         closeNav();
-        pfeil.style.left = 0;
-        pfeil.innerHTML = "→"
     }
 }
 
