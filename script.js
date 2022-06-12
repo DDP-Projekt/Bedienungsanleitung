427ef41dddc137dca383fabe2bfbe7a6e1521778
NotLe0n <notle0n75@gmail.com>
2022-06-12 13:32:37 +0200

    Autofill href attibutes, more comments

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const md = document.getElementById("md");
+    // get all a tags inside an ordered list item.
+    const links = document.querySelectorAll("ol li a");
+    // if there is no href tag defined, add one using the contents of the a tag.
+    links.forEach(x => !x.getAttribute("href") ? x.setAttribute("href", "?p=" + x.innerHTML.replace("&amp;", "&")) : x);
+
+    const md = document.getElementById("md"); // zero-md element
+    // point source to the correct md file using the query contents. If the query is empty, use the start page.
     md.setAttribute("src", !window.location.search ? "Artikel/Startseite.md" : "Artikel/" + window.location.search.slice(3) + ".md");
 
+    // apply syntax highlighting once zero-md and Prism.js are loaded
     md.addEventListener('zero-md-ready', () => {
         applySyntaxHighlighting();
     });
@@ -10,17 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
 let sidebarHidden = false;
 
 function openNav() {
+    // show sidebar
     sidebarHidden = false;
     document.getElementById("seitenleiste").style.display = "flex";
     
+    // push the hamburger button to the end of the sidebar.
     const burger = document.getElementById("hamburger");
     burger.style.left = "calc(var(--sidebar-size) - 20px)";
 }
 
 function closeNav() {
+    // hide sidebar
     sidebarHidden = true;
     document.getElementById("seitenleiste").style.display = "none";
     
+    // set hamburger button to the left of the screen
     const burger = document.getElementById("hamburger");
     burger.style.left = "10px";
 }
@@ -35,6 +46,7 @@ function toggleNav() {
 }
 
 function applySyntaxHighlighting() {
+    // regex rules for syntax highlighting
     Prism.languages['ddp'] = {
         'placeholder': /<[^<>]+>/,
         'comment': /\[[\s\S]*\]/,
