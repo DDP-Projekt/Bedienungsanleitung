7bddc20782ca5479a0261faa882960490df7d7a5
NotLe0n <notle0n75@gmail.com>
2022-08-27 20:06:21 +0200

    Added language setting

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // get all a tags inside an ordered list item.
-    const links = document.querySelectorAll("ol li a");
-    // if there is no href tag defined, add one using the contents of the a tag.
-    links.forEach(x => !x.getAttribute("href") ? x.setAttribute("href", "?p=" + x.innerHTML.replace("&amp;", "&")) : x);
+var searchParams = new URLSearchParams(window.location.search);
+if (searchParams.get('p') === null) {
+	searchParams.set('p', 'Startseite')
+	window.location.search = searchParams.toString();
+}
 
-    const md = document.getElementById("md"); // zero-md element
-    // point source to the correct md file using the query contents. If the query is empty, use the start page.
-    md.setAttribute("src", !window.location.search ? "Artikel/Startseite.md" : "Artikel/" + window.location.search.slice(3) + ".md");
+if (searchParams.get('lang') === null) {
+	searchParams.set('lang', 'DE')
+	window.location.search = searchParams.toString();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	populateListItems();
+
+	populateMDElement();
 
     // apply syntax highlighting once zero-md and Prism.js are loaded
     md.addEventListener('zero-md-ready', () => {
         applySyntaxHighlighting();
     });
 });
+
+function populateListItems() {
+	var searchParams = new URLSearchParams(window.location.search);
+
+    // get all a tags inside an ordered list item.
+    const links = document.querySelectorAll("ol li a");
+    // if there is no href tag defined, add one using the contents of the a tag.
+	for (let i = 0; i < links.length; i++) {
+		const link = links[i];
+		if (!link.getAttribute("href")) {
+			link.setAttribute("href", "?p=" + link.innerHTML + "&lang=" + searchParams.get('lang'))
+		}
+	}
+}
+
+function populateMDElement() {
+	var searchParams = new URLSearchParams(window.location.search);
+	const destinationPage = "Artikel/" + searchParams.get('lang') + '/' + searchParams.get('p') + ".md";
+
+    const md = document.getElementById("md"); // zero-md element
+    // point source to the correct md file using the query contents. If the query is empty, use the start page.
+    md.setAttribute("src", searchParams.get('p') === null ? startPage : destinationPage);
+}
 
 let sidebarHidden = false;
 
@@ -60,4 +89,12 @@ function applySyntaxHighlighting() {
         'operator': /\b(ist|sind|oder|und|nicht|plus|minus|mal|durch|modulo|hoch|. wurzel von|logisch|kontra|gleich|ungleich|kleiner als|größer als|kleiner als, oder|größer als, oder|natürlicher Logarithmus von|Betrag|Stück|von|bis|als|an der Stelle)\b/,
         'type': /nichts|Typ|Funktion|Zahl(en)?|Kommazahl(en)?|Booleans?|Texte?|Buchstaben?|[Dd]er|[Dd]ie|[Dd]as/,
     };
+}
+
+function changeLanguage(lang) {
+	const languages = ['DE', 'EN']
+
+	var searchParams = new URLSearchParams(window.location.search);
+	searchParams.set("lang", languages[lang]);
+    window.location.search = searchParams.toString();
 }
