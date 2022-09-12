5cb4e8e037d62d589500a068cb27555ea732687e
NotLe0n <notle0n75@gmail.com>
2022-09-12 22:35:33 +0200

    changed Artikel folder structure, added IDEA folder

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var searchParams = new URLSearchParams(window.location.search);
+'use strict'
+
+const searchParams = new URLSearchParams(window.location.search);
 if (searchParams.get('p') === null) {
 	searchParams.set('p', 'Startseite')
 	window.location.search = searchParams.toString();
@@ -10,7 +12,6 @@ if (searchParams.get('lang') === null) {
 }
 
 customElements.define('to-do', TODOElement);
-
 document.addEventListener("DOMContentLoaded", () => {
 	populateListItems();
 
@@ -23,26 +24,57 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function populateListItems() {
-	var searchParams = new URLSearchParams(window.location.search);
+	let searchParams = new URLSearchParams(window.location.search);
+    let articleLinks = document.getElementById('article-links')
 
-    // get all a tags inside an ordered list item.
-    const links = document.querySelectorAll("ol li a");
-    // if there is no href tag defined, add one using the contents of the a tag.
-	for (let i = 0; i < links.length; i++) {
-		const link = links[i];
-		if (!link.getAttribute("href")) {
-			link.setAttribute("href", "?p=" + link.innerHTML + "&lang=" + searchParams.get('lang'))
-		}
-	}
+    traverseListElement(articleLinks, function (element) {
+        // only allow <a> tags without a href attribute
+        if (element.nodeName !== "A" || element.getAttribute("href")) {
+            return;
+        }
+
+        // add href attribute
+        element.setAttribute("href", `?p=${path}${element.innerHTML}&lang=${searchParams.get('lang')}`)
+    })
+}
+
+let path = "";
+
+/**
+ * Super janky lol
+ * @param {Element} element The Element
+ * @param {function(Element)} callback
+ */
+function traverseListElement(element, callback) {
+    // only allow elements with children
+    if (element == null || element.children.length === 0) {
+        return;
+    }
+
+    // loop through children
+    for (const child of element.children) {
+        if (child.nodeName === "OL") {
+            // add the innerHTML of the <a> tag above the <ol> tag to the path
+            path += child.parentElement.firstElementChild.innerHTML + "/";
+        }
+
+        callback(child);
+        traverseListElement(child, callback);
+    }
+
+    // remove old path
+    if (element === element.parentElement.lastElementChild) {
+        path = path.replace(element.parentElement.firstElementChild.innerHTML + "/", "");
+    }
 }
 
 function populateMDElement() {
-	var searchParams = new URLSearchParams(window.location.search);
-	const destinationPage = "Artikel/" + searchParams.get('lang') + '/' + searchParams.get('p') + ".md";
+    const searchParams = new URLSearchParams(window.location.search);
+    const destinationPage = "Artikel/" + searchParams.get('lang') + '/' + searchParams.get('p') + ".md";
 
     const md = document.getElementById("md"); // zero-md element
     // point source to the correct md file using the query contents. If the query is empty, use the start page.
-    md.setAttribute("src", searchParams.get('p') === null ? startPage : destinationPage);
+    md.setAttribute("src", searchParams.get('p') === null ? "Startseite" : destinationPage);
 }
 
 let sidebarHidden = false;
@@ -80,14 +112,14 @@ function applySyntaxHighlighting() {
     // regex rules for syntax highlighting
     Prism.languages['ddp'] = {
         'placeholder': /<[^<>]+>/,
-        'comment': /\[[\s\S]*?\]/,
+        'comment': /\[[\s\S]*?]/,
         'builtins': /Schreibe (den|die) (Zahl(en)?|Kommazahl(en)?|Buchstaben?|Texte?)|auf eine Zeile/,
-        'string-literal': /\".*\"/,
-        'char-literal': /\'.*\'/,
+        'string-literal': /".*"/,
+        'char-literal': /'.*'/,
         'boolean-literal': /wahr|falsch/,
         'number': /-?\d+(,\d)?/,
         'control-statement': /[Ww]enn|aber|dann|Sonst|[Ss]olange|[Mm]ache|Für|jede|mit Schrittgröße|[Gg]ibt?|zurück|macht|einen?|mit den Parametern|vom|und kann so benutzt werden|Binde|ein/,
-        'punctuation': /\.|\:/,
+        'punctuation': /[.:]/,
         'operator': /\b(ist|sind|oder|und|nicht|plus|minus|mal|durch|modulo|hoch|. wurzel von|logisch|kontra|gleich|ungleich|kleiner als|größer als|kleiner als, oder|größer als, oder|natürlicher Logarithmus von|Betrag|Stück|von|bis|als|an der Stelle)\b/,
         'type': /nichts|Typ|Funktion|Zahl(en)?|Kommazahl(en)?|Booleans?|Texte?|Buchstaben?|[Dd]er|[Dd]ie|[Dd]as/,
     };
@@ -96,7 +128,7 @@ function applySyntaxHighlighting() {
 function changeLanguage(lang) {
 	const languages = ['DE', 'EN']
 
-	var searchParams = new URLSearchParams(window.location.search);
-	searchParams.set("lang", languages[lang]);
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("lang", languages[lang]);
     window.location.search = searchParams.toString();
 }
