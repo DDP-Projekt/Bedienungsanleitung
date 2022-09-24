19c1cd1b762000e47b6494ce5358f62e2e5002fe
NotLe0n <notle0n75@gmail.com>
2022-09-24 20:33:46 +0200

    better todo elm styling

--- elements/todoElement.js
+++ elements/todoElement.js
@@ -7,19 +7,6 @@ class TODOElement extends HTMLElement {
 		this.attachShadow({mode: 'open'});
 
 		const div = document.createElement('div');
-		div.style = `
-						width: 99%;
-						display: flex;
-						height: 50px;
-						border: 5px solid #df5e5e;
-						background-color: #303336;
-						justify-content: center;
-						align-content: center;
-						border-radius: 15px;
-					`;
-
-
-
 		const text = document.createElement('p');
 		text.innerHTML = "An diesem Artikel muss noch gearbeitet werden!";
 		div.appendChild(text);
