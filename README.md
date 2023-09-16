62ba09654a64612f7c44b1d0d9025975aea269d1
NotLe0n <notle0n75@gmail.com>
2023-09-16 15:06:22 +0200

    added https support and config

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ Die Dokumentation für die Deutsche Programmiersprache
 
 
 Die einzelnen Artikel liegen im Ordner "Artikel" und sind Markdown Dateien. Ein Javascript script wandelt dieses Markdown in HTML um, sodass es auf der Webseite angezeigt werden kann.
+
+## Lokal Ausführen
+### Vorraussetzungen
+1. Go
+
+### Starten
+1. Git Repository clonen
+2. `config.json` erstellen (Inhalt für Standardwerte: "{}")
+3. `go run ./server`
+
+### Konfiguration
+Die Standardkonfiguration sieht so aus:
+````json
+{
+	"port": "8080",
+	"useHTTPS": false,
+	"certPath": "",
+	"keyPath": ""
+}
+``
--- server/server.go
+++ server/server.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
+func setup_config() {
+	viper.SetDefault("port", "8080")
+	viper.SetDefault("useHTTPS", false)
+	viper.SetDefault("certPath", "")
+	viper.SetDefault("keyPath", "")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s\n", err)
+	}
+
+	json_string, err := json.MarshalIndent(viper.AllSettings(), "", "\t")
+	if err == nil {
+		log.Printf("using config: \n%s\n", json_string)
+	}
+}
+
 func main() {
+	setup_config()
+
 	r := gin.Default()
 
 	r.LoadHTMLGlob("server/html/*")
@@ -41,5 +65,13 @@ func main() {
 		})
 	})
 
-	r.Run()
+	// run the server
+	if viper.GetBool("useHTTPS") {
+		if viper.GetString("certPath") == "" || viper.GetString("keyPath") == "" {
+			log.Fatal("certPath and keyPath can not be empty!")
+		}
+		log.Fatal(r.RunTLS(":"+viper.GetString("port"), viper.GetString("certPath"), viper.GetString("keyPath")))
+	} else {
+		log.Fatal(r.Run(":" + viper.GetString("port")))
+	}
 }
