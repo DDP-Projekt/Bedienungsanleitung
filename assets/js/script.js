88cba449a1025167e6421e3cb1046648f16f2023
NotLe0n <notle0n75@gmail.com>
2024-05-30 14:25:43 +0200

    fixed new prev and next buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,29 +22,3 @@ function toggleNav() {
 	sidebar.classList.toggle('opened');
 	burger.classList.toggle('opened');
 }
-
-// -1 = prev, 1 = next
-function goToSurroundingLink(dir) {
-	const anchors = document.querySelectorAll('#artikel-links a');
-	let currentIndex = -1;
-
-	// Find the index of the current position
-	anchors.forEach(function(anchor, index) {
-		if (anchor.href === window.location.href) {
-			currentIndex = index;
-		}
-	});
-
-	if (dir === -1) {
-		if (currentIndex <= 0) {
-			currentIndex = anchors.length;
-		}
-		window.location.href = anchors[currentIndex - 1].getAttribute('href');
-	}
-	else if (dir === 1) {
-		if (currentIndex >= anchors.length - 1) {
-			currentIndex = -1;
-		}
-		window.location.href = anchors[currentIndex + 1].getAttribute('href');
-	}
-}
