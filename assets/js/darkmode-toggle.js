94fd5f41baebd1d460245410c2a5cb435c7467a7
NotLe0n <notle0n75@gmail.com>
2024-06-07 19:03:01 +0200

    made title an svg, removing need for font

--- assets/js/darkmode-toggle.js
+++ assets/js/darkmode-toggle.js
@@ -1,9 +1,11 @@
 if (window.localStorage.getItem("dark-mode") === "false") {
-	document.querySelector('#dark').media = "not all";
-	document.querySelector('#light').media = "all";
+	document.querySelector('#dark').media = 'not all';
+	document.querySelector('#light').media = 'all';
+	window.localStorage.setItem("dark-mode", "false");
 } else {
-	document.querySelector('#dark').media = "all";
-	document.querySelector('#light').media = "not all";
+	document.querySelector('#dark').media = 'all';
+	document.querySelector('#light').media = 'not all';
+	window.localStorage.setItem("dark-mode", "true");
 }
 
 function toggleDarkMode() {
@@ -11,11 +13,13 @@ function toggleDarkMode() {
 	if (isDark) {
 		document.querySelector('#dark').media = 'not all';
 		document.querySelector('#light').media = 'all';
+		document.querySelector('#titel').src = 'img/title-dark.svg';
 		window.localStorage.setItem("dark-mode", "false");
 	}
 	else {
 		document.querySelector('#dark').media = 'all';
 		document.querySelector('#light').media = 'not all';
+		document.querySelector('#titel').src = 'img/title.svg';
 		window.localStorage.setItem("dark-mode", "true");
 	}
 }
